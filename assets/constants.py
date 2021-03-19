69ce4de2cdec6990f806debf902f0edaaf3baa23
hermancai <30858461+hermancai@users.noreply.github.com>
2021-03-18 18:19:19 -0700

    remove replit values

--- assets/constants.py
+++ assets/constants.py
@@ -7,10 +7,6 @@ PEARL_COUNT = 4
 # dimensions
 WIDTH, HEIGHT = 560, 560
 SMALL_FONT, LARGE_FONT = 18, 24
-
-# WIDTH, HEIGHT = 400, 400  # for fitting into repl.it
-# SMALL_FONT, LARGE_FONT = 13, 18  # for fitting into repl.it
-
 WIN_WIDTH, WIN_HEIGHT = WIDTH * 2, HEIGHT
 ROWS, COLS = 7, 7
 SQUARE_SIZE = WIDTH//COLS
